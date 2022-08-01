26b50bc4e7d865381fb354b2ce056d570afb25f2
Guembe Blessing <blessingodede@gmail.com>
2022-08-01 23:15:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -505,7 +505,7 @@ def create_artist_form():
 def create_artist_submission():
   # called upon submitting the new artist listing form
   # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
+  
     skeeK_venue = json.loads(request.form['seeking_venue'].lower())
     try:
         insert_artist = Artist(
